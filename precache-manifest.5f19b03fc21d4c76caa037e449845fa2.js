2457fc9ba645c162cc203ca5c04fefcd3aa7818b
lnd4812 <laureldavid64@gmail.com>
2022-07-11 09:56:29 -0400

    Updates

---
+++ precache-manifest.5f19b03fc21d4c76caa037e449845fa2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "89a1123e2ce86142c41b",
+    "url": "/mod-20-react-portfolio/static/css/main.8c8d1b54.chunk.css"
+  },
+  {
+    "revision": "89a1123e2ce86142c41b",
+    "url": "/mod-20-react-portfolio/static/js/main.89a1123e.chunk.js"
+  },
+  {
+    "revision": "41211fcf000cf3a9973c",
+    "url": "/mod-20-react-portfolio/static/js/1.41211fcf.chunk.js"
+  },
+  {
+    "revision": "512396de91e974b59005",
+    "url": "/mod-20-react-portfolio/static/js/2.512396de.chunk.js"
+  },
+  {
+    "revision": "97e5b3037266037d3542",
+    "url": "/mod-20-react-portfolio/static/js/runtime~main.97e5b303.js"
+  },
+  {
+    "revision": "ea56d67dd89de229e4e90a5e432551df",
+    "url": "/mod-20-react-portfolio/static/media/theme.ea56d67d.png"
+  },
+  {
+    "revision": "f92d9d5177a2858ecc6a25eda3e7d689",
+    "url": "/mod-20-react-portfolio/static/media/LND2021.f92d9d51.png"
+  },
+  {
+    "revision": "fe492c3246bb2d0059b44c5244f08e5a",
+    "url": "/mod-20-react-portfolio/static/media/resumepdf.fe492c32.pdf"
+  },
+  {
+    "revision": "e1184d2d54e285dbf90013edc5d32725",
+    "url": "/mod-20-react-portfolio/static/media/budget-app.e1184d2d.png"
+  },
+  {
+    "revision": "2d7e87bdd8aa80d1432c1ed553917806",
+    "url": "/mod-20-react-portfolio/static/media/food-finder-group-project-1.2d7e87bd.png"
+  },
+  {
+    "revision": "0a25ddc81d117f5a225773ba9395aa32",
+    "url": "/mod-20-react-portfolio/static/media/password-generator.0a25ddc8.png"
+  },
+  {
+    "revision": "c04712ce6c9fd6ad8190f278c535cce5",
+    "url": "/mod-20-react-portfolio/static/media/tech-blog.c04712ce.png"
+  },
+  {
+    "revision": "e9136030c80247da309e991ca7c1a59e",
+    "url": "/mod-20-react-portfolio/static/media/weather-dashboard.e9136030.png"
+  },
+  {
+    "revision": "7ee74548c839741108eabe0b2ef49526",
+    "url": "/mod-20-react-portfolio/static/media/workday-scheduler.7ee74548.png"
+  },
+  {
+    "revision": "61169eff7453f19eee3975aaa8fcc01b",
+    "url": "/mod-20-react-portfolio/index.html"
+  }
+];
